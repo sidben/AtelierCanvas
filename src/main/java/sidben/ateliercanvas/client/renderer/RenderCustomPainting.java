3838eed879aeac49bb71d71a6676c064c942c106
sidben <sidbenetti@gmail.com>
2015-03-28 17:03:52 -0300

    TEST - loading image from url

--- src/main/java/sidben/ateliercanvas/client/renderer/RenderCustomPainting.java
+++ src/main/java/sidben/ateliercanvas/client/renderer/RenderCustomPainting.java
@@ -1,13 +1,23 @@
 package sidben.ateliercanvas.client.renderer;
 
+import java.awt.Color;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.imageio.ImageIO;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL12;
 import sidben.ateliercanvas.entity.item.EntityCustomPainting;
 import sidben.ateliercanvas.helper.LogHelper;
+import sidben.ateliercanvas.proxy.ClientProxy;
+import sidben.ateliercanvas.world.storage.PaintingData;
 import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.client.renderer.texture.DynamicTexture;
+import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.entity.Entity;
 import net.minecraft.init.Blocks;
@@ -27,6 +37,8 @@ public class RenderCustomPainting extends Render
     
 
     
+
+    
     @Override
     public void doRender(Entity p_76986_1_, double p_76986_2_, double p_76986_4_, double p_76986_6_, float p_76986_8_, float p_76986_9_)
     {
@@ -43,7 +55,9 @@ public class RenderCustomPainting extends Render
     
     protected ResourceLocation getEntityTexture(EntityCustomPainting entity)
     {
-        return this.vabillaPainting;
+        return ClientProxy.customPainting;
+        
+        // return this.vabillaPainting;
     }
     
     
@@ -65,6 +79,10 @@ public class RenderCustomPainting extends Render
         LogHelper.info("    painting direction: " + painting.hangingDirection);
         LogHelper.info("    painting field: " + painting.field_146063_b + ", " + painting.field_146064_c + ", " + painting.field_146062_d);
         LogHelper.info("    offset: " + Direction.offsetX[painting.hangingDirection] + ", " + Direction.offsetZ[painting.hangingDirection]);
+        
+        
+        
+        
         
         
         
@@ -112,10 +130,16 @@ public class RenderCustomPainting extends Render
         tessellator.startDrawingQuads();
         
         tessellator.setNormal(0.0F, 0.0F, -1.0F);
+        /*
         tessellator.addVertexWithUV(f + 16, f1 + 0, (double)(-f2), 1, 0);
         tessellator.addVertexWithUV(f + 0, f1 + 0, (double)(-f2), 0, 0);
         tessellator.addVertexWithUV(f + 0, f1 + 16, (double)(-f2), 0, 1);
         tessellator.addVertexWithUV(f + 16, f1 + 16, (double)(-f2), 1, 1);
+        */
+        tessellator.addVertexWithUV(f + 16, f1 + 0, (double)(-f2), 0, 1);
+        tessellator.addVertexWithUV(f + 0, f1 + 0, (double)(-f2), 1, 1);
+        tessellator.addVertexWithUV(f + 0, f1 + 16, (double)(-f2), 1, 0);
+        tessellator.addVertexWithUV(f + 16, f1 + 16, (double)(-f2), 0, 0);
         
         tessellator.draw();
 
--- src/main/java/sidben/ateliercanvas/proxy/ClientProxy.java
+++ src/main/java/sidben/ateliercanvas/proxy/ClientProxy.java
@@ -1,12 +1,21 @@
 package sidben.ateliercanvas.proxy;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.imageio.ImageIO;
 import cpw.mods.fml.client.registry.RenderingRegistry;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.DynamicTexture;
+import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import sidben.ateliercanvas.client.renderer.RenderCustomPainting;
 import sidben.ateliercanvas.entity.item.EntityCustomPainting;
 import sidben.ateliercanvas.helper.LogHelper;
 import sidben.ateliercanvas.init.MyItems;
+import sidben.ateliercanvas.world.storage.PaintingData;
 
 
 
@@ -14,6 +23,14 @@ import sidben.ateliercanvas.init.MyItems;
 public class ClientProxy extends CommonProxy
 {
 
+    static final Minecraft mc = Minecraft.getMinecraft();
+    private static PaintingData customData;
+    private static DynamicTexture texture;
+    public static ResourceLocation customPainting;
+    private static int[] pixels;
+
+    
+    
 
     @Override
     public void pre_initialize()
@@ -27,6 +44,15 @@ public class ClientProxy extends CommonProxy
         MyItems.registerRender();
 
         // Special renderers
+        
+        
+        
+        
+        
+
+        
+
+        
     }
 
 
@@ -44,6 +70,67 @@ public class ClientProxy extends CommonProxy
         RenderingRegistry.registerEntityRenderingHandler(EntityCustomPainting.class, new RenderCustomPainting());
 
         
+        
+        
+        
+        
+        
+        
+        
+        
+        TextureManager texMan = mc.renderEngine;
+
+        customData = null;
+        texture = new DynamicTexture(16, 16);
+        pixels = texture.getTextureData();
+        
+        LogHelper.info("]   " + pixels.length);
+
+        /*
+        for (int i = 0; i < pixels.length; ++i)
+        {
+            //this.pixels[i] = 0;
+            //this.pixels[i] = new Color(255, 128, 0).getRGB();
+            pixels[i] = 16742144;
+        }
+        
+        texture.updateDynamicTexture();
+        */
+        
+        /*
+        String auxPX = "";
+        for (int i = 0; i < 16; ++i) {
+            auxPX += pixels[i] + ", ";
+        }
+        LogHelper.info("]   " + auxPX);
+        */
+        
+        //this.customPainting = texMan.getDynamicTextureLocation("custom_painting", texture);
+
+        
+        
+        URL url;
+        try {
+            
+            url = new URL("http://38.media.tumblr.com/avatar_dce0043e4cf6_16.png");
+            texture = new DynamicTexture(ImageIO.read(url));
+            customPainting = texMan.getDynamicTextureLocation("test", texture);
+            
+            
+        } catch (MalformedURLException e1) {
+            e1.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        
+        pixels = texture.getTextureData();
+        String auxPX = "";
+        for (int i = 0; i < 16; ++i) {
+            auxPX += pixels[i] + ", ";
+        }
+        LogHelper.info("]   " + auxPX);
+
     }
 
 
