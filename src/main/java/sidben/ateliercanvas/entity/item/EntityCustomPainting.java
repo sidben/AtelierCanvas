ddccccde9595e085c50a15d5be6e19929124cea5
sidben <sidbenetti@gmail.com>
2015-09-22 01:35:47 -0300

    Bounding box updates on client (fixed #6)
    Hitbox now works properly, I just had to call the setDirection method on
    client.
    
    I need to pass the coords and the direction on the packet, since the
    client won't have that info when the custom painting spawn.

--- src/main/java/sidben/ateliercanvas/entity/item/EntityCustomPainting.java
+++ src/main/java/sidben/ateliercanvas/entity/item/EntityCustomPainting.java
@@ -1,113 +1,105 @@
 package sidben.ateliercanvas.entity.item;
 
-import java.util.Date;
 import io.netty.buffer.ByteBuf;
-import cpw.mods.fml.common.network.ByteBufUtils;
-import cpw.mods.fml.common.registry.IEntityAdditionalSpawnData;
-import sidben.ateliercanvas.handler.ConfigurationHandler;
-import sidben.ateliercanvas.handler.CustomPaintingConfigItem;
-import sidben.ateliercanvas.helper.LogHelper;
-import sidben.ateliercanvas.init.MyItems;
+import java.util.Date;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityHanging;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
+import sidben.ateliercanvas.handler.ConfigurationHandler;
+import sidben.ateliercanvas.handler.CustomPaintingConfigItem;
+import sidben.ateliercanvas.init.MyItems;
+import cpw.mods.fml.common.network.ByteBufUtils;
+import cpw.mods.fml.common.registry.IEntityAdditionalSpawnData;
 
 
 
-public class EntityCustomPainting extends EntityHanging implements IEntityAdditionalSpawnData 
+public class EntityCustomPainting extends EntityHanging implements IEntityAdditionalSpawnData
 {
-    
+
     private CustomPaintingConfigItem _entry;
-        
-    
+
+
     public EntityCustomPainting(World world) {
         super(world);
-        
-        LogHelper.info("EntityCustomPainting()");
-        LogHelper.info("-   entry null: " + (_entry == null));
     }
-    
-    
+
+
     public EntityCustomPainting(World world, int x, int y, int z, int direction, String uuid) {
         super(world, x, y, z, direction);
         this.setPaintingEntry(uuid);
         this.setDirection(direction);
-        
-        LogHelper.info("EntityCustomPainting()");
-        LogHelper.info(".   entry null: " + (_entry == null));
-        LogHelper.info(".   uuid: " + uuid);
-        LogHelper.info(".   pos: " + x + ", " + y + "," + z);
     }
-    
-    
-    
+
+
+
     /**
      * Loads the config info for a given UUID, or a generic placeholder
-     * if the config info is invalid or not found.  
+     * if the config info is invalid or not found.
      * 
      * @param uuid
      */
-    private void setPaintingEntry(String uuid) {
-        LogHelper.info("setPaintingEntry('" + uuid + "')");
-        
+    private void setPaintingEntry(String uuid)
+    {
         final CustomPaintingConfigItem paintingConfig = ConfigurationHandler.findPaintingByUUID(uuid);
         if (paintingConfig != null) {
             this._entry = paintingConfig;
         } else {
-            this._entry = new CustomPaintingConfigItem("", uuid, false, 0, "", "",  new Date(), new Date());
+            this._entry = new CustomPaintingConfigItem("", uuid, false, 0, "", "", new Date(), new Date());
         }
-
-        LogHelper.info(".   entry: " + this._entry.toString());
     }
-    
-    
-    
 
-    public String getImageUUID() {
-        if (this._entry == null) return "";
+
+
+    public String getImageUUID()
+    {
+        if (this._entry == null) {
+            return "";
+        }
         return this._entry.getUUID();
     }
-    
+
     @Override
     public int getWidthPixels()
     {
-        if (this._entry == null) return 16;
+        if (this._entry == null) {
+            return 16;
+        }
         return this._entry.getTileWidth() * ConfigurationHandler.defaultResolution;
     }
 
     @Override
     public int getHeightPixels()
     {
-        if (this._entry == null) return 16;
+        if (this._entry == null) {
+            return 16;
+        }
         return this._entry.getTileHeight() * ConfigurationHandler.defaultResolution;
     }
 
     @Override
     public void onBroken(Entity entity)
     {
-        if (entity instanceof EntityPlayer)
-        {
-            EntityPlayer player = (EntityPlayer)entity;
-            if (player.capabilities.isCreativeMode) return;
+        if (entity instanceof EntityPlayer) {
+            final EntityPlayer player = (EntityPlayer) entity;
+            if (player.capabilities.isCreativeMode) {
+                return;
+            }
         }
 
         this.entityDropItem(new ItemStack(MyItems.customPainting), 0.0F);
     }
 
-    
-    
-    
+
+
     /**
      * (abstract) Protected helper method to write subclass entity data to NBT.
      */
+    @Override
     public void writeEntityToNBT(NBTTagCompound nbtTagCompound)
     {
-        LogHelper.info("writeEntityToNBT()");
-        LogHelper.info("    :" + this.getImageUUID());
-
         nbtTagCompound.setString("uuid", this.getImageUUID());
         super.writeEntityToNBT(nbtTagCompound);
     }
@@ -115,34 +107,54 @@ public class EntityCustomPainting extends EntityHanging implements IEntityAdditi
     /**
      * (abstract) Protected helper method to read subclass entity data from NBT.
      */
+    @Override
     public void readEntityFromNBT(NBTTagCompound nbtTagCompound)
     {
-        LogHelper.info("readEntityFromNBT()");
+        final String uniqueId = nbtTagCompound.getString("uuid");
 
-        String uniqueId = nbtTagCompound.getString("uuid");
-        
-        LogHelper.info("    :" + uniqueId);
-        
         this.setPaintingEntry(uniqueId);
         super.readEntityFromNBT(nbtTagCompound);
     }
-    
+
 
     @Override
     public void writeSpawnData(ByteBuf buffer)
     {
-        LogHelper.info("writeSpawnData()");
-        LogHelper.info("    $" + this.getImageUUID());
+        /*
+         * Sends the painting info to the client. I need to send the direction and
+         * XYZ coordinates because the client won't have the correct data when the 
+         * entity spawns.
+         * 
+         *  This behaves like the vanilla [handleSpawnPainting], that handles packets
+         *  client-side when an EntityPainting spawns.
+         */
         ByteBufUtils.writeUTF8String(buffer, this.getImageUUID());
+        buffer.writeByte(hangingDirection);
+        buffer.writeInt(this.field_146063_b);       // x
+        buffer.writeInt(this.field_146064_c);       // y
+        buffer.writeInt(this.field_146062_d);       // z
     }
 
     @Override
     public void readSpawnData(ByteBuf buffer)
     {
-        LogHelper.info("readSpawnData()");
-        String uniqueId = ByteBufUtils.readUTF8String(buffer);
-        LogHelper.info("    $" + uniqueId);
+        /*
+         * Reads the entity info from the server and updates the client.
+         * 
+         * First I need to read the UUID of the painting, so the object can be load by
+         * the [setPaintingEntry] method.
+         * 
+         * Then I need to set the correct XYZ coordinates and call the [setDirection] 
+         * method in order to create the correct bounding box.
+         */
+        final String uniqueId = ByteBufUtils.readUTF8String(buffer);
+        final int direction = buffer.readByte();
+        this.field_146063_b = buffer.readInt();     // x
+        this.field_146064_c = buffer.readInt();     // y
+        this.field_146062_d = buffer.readInt();     // z
+
         this.setPaintingEntry(uniqueId);
+        this.setDirection(direction);
     }
 
 
