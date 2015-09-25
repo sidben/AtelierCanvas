sidben/AtelierCanvas

commit 183dec5e13570bbe9adc4ea03cf975bc4700273b

Refactor for the config (broke rendering)
Organized some classes and tried to group all config manipulation to the
actual ConfigurationHandler class.

I also implemented a proper UUID instead of string for the painting UUID
and made the Mysterious Painting only gives enabled paintings.

Sadly paintings don't render anymore, it's probably the entity not
saving/loading the UUID correctly.

src/main/java/sidben/ateliercanvas/handler/ConfigurationHandler.java
@@ -5,10 +5,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.UUID;
-import net.minecraft.util.StringUtils;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.common.config.ConfigCategory;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.common.config.Property;
+import scala.actors.threadpool.Arrays;
+import sidben.ateliercanvas.helper.ImageFilenameFilter;
 import sidben.ateliercanvas.helper.LogHelper;
 import sidben.ateliercanvas.reference.Reference;
 import cpw.mods.fml.client.config.GuiConfigEntries;
@@ -19,55 +21,62 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 public class ConfigurationHandler
 {
 
-    public static final String                   CATEGORY_PAINTINGS         = "paintings";
-    public static final String                   CATEGORY_IMPORT            = "paintings_import";
-    public static final String                   CATEGORY_EXPORT            = "paintings_export";
-    public static final String                   PAINTINGS_ARRAY_KEY        = "painting_list";
-    public final static String                   IMAGES_BASE_PATH           = "config/AtelierCanvas_Paintings/";
-    
+    public static final String                    CATEGORY_PAINTINGS         = "paintings";
+    public static final String                    CATEGORY_IMPORT            = "paintings_import";
+    public static final String                    CATEGORY_EXPORT            = "paintings_export";
+    public static final String                    PAINTINGS_ARRAY_KEY        = "painting_list";
+    public static final String                    IMAGES_BASE_PATH           = "config/AtelierCanvas_Paintings/";
+    public static final UUID                      EMPTY_UUID                 = UUID.fromString("00000000-0000-0000-0000-000000000000");
+
 
 
     /** List with info of all paintings installed */
-    public static List<CustomPaintingConfigItem> mahPaintings;
+    private static List<CustomPaintingConfigItem> mahPaintings;
+
+    /** List with UUIDs of enabled paintings */
+    private static List<UUID>                     visiblePaintings;
 
     /** Largest file size (in bytes) accepted by the mod */
-    public static long                           maxFileSize;
+    public static long                            maxFileSize;
 
     /** Format in which the painting dates (created / last updated) will be displayed */
-    public static String                         paintingDateFormat;
+    public static String                          paintingDateFormat;
 
     /** Maximum painting size, in pixels, accepted by the mod. */
-    public static int                            maxPaintingSize;
+    public static int                             maxPaintingSize;
 
     /** Default resolution of paintings */
-    public static final int                      defaultResolution          = 16;
+    public static final int                       minPaintingSize            = 16;
+
+    /** Default resolution of paintings */
+    public static final int                       defaultResolution          = 16;
 
     /** Maximum amount of paintings cached */
-    public static final int                      maxPaintings               = 256;
+    public static final int                       maxPaintings               = 256;
 
     /** Time in minutes a painting will remain in cache */
-    public static int                            expirationTime             = 10;
+    public static final int                       expirationTime             = 10;
 
     /** Chance of a random painting giving an original/authentic painting */
-    public static int                            chanceAuthentic            = 5;
+    public static final int                       chanceAuthentic            = 5;
 
     /** Chance of a random painting giving a forgery */
-    public static int                            chanceForgery              = 15;
+    public static final int                       chanceForgery              = 15;
 
     /** Activates simpler crafting recipes, if the player plays on peaceful or just wants to use paintings quickly */
-    public static boolean                        simpleRecipes;
+    public static boolean                         simpleRecipes;
 
-    
 
-    private static final int                     DEFAULT_maxFileSize        = 30;             // NOTE: the default file size is in KBytes to make the property slider more friendly
-    private static final int                     DEFAULT_maxPaintingSize    = 64;
-    private static final String                  DEFAULT_paintingDateFormat = "yyyy-MM-dd";
-    private static final boolean                 DEFAULT_simpleRecipes      = false;
+
+    private static final int                      DEFAULT_maxFileSize        = 30;                               // NOTE: the default file size is in KBytes to make the property slider more friendly
+    private static final int                      DEFAULT_maxPaintingSize    = 64;
+    private static final String                   DEFAULT_paintingDateFormat = "yyyy-MM-dd";
+    private static final boolean                  DEFAULT_simpleRecipes      = false;
 
 
 
     // Instance
-    public static Configuration                  config;
+    public static Configuration                   config;
 
 
 
@@ -128,6 +137,7 @@ public class ConfigurationHandler
         // Loads custom paintings
         LogHelper.info("Loading custom paintings config info...");
         mahPaintings = new ArrayList<CustomPaintingConfigItem>();
+        visiblePaintings = new ArrayList<UUID>();
 
         for (final Property item : cat.getOrderedValues()) {
             if (item.getName().startsWith(PAINTINGS_ARRAY_KEY)) {
@@ -136,6 +146,9 @@ public class ConfigurationHandler
 
                 if (configItem.isValid()) {
                     mahPaintings.add(configItem);
+                    if (configItem.getIsEnabled()) {
+                        visiblePaintings.add(configItem.getUUID());
+                    }
                 } else {
                     LogHelper.info("    Error loading a config entry: [" + configItem.getValiadtionErrors() + "]");
                 }
@@ -173,58 +186,186 @@ public class ConfigurationHandler
         return "sidben.ateliercanvas.config.prop." + name;
     }
 
-    
-    
-    
+
+
+    // -------------------------------------------------------------------------
+    // Custom Paintings properties - Currently installed
+    // -------------------------------------------------------------------------
+
     /**
-     * Looks on the current custom paintings installed for an element with the given UUID. 
+     * <p>
+     * Returns all the paintings that were loaded from the config file.
+     * </p>
+     * <p>
+     * This means all paintings which <b>config is valid</b> (have all required fields), but it returns paintings enabled and disabled. At this point, the config makes no check if the files actually exists or have a proper size, those rules are checked before rendering the painting.
+     * </p>
      * 
-     * @param uuid UUID in string format
+     * @see sidben.ateliercanvas.helper.AtelierHelper
+     */
+    public static List<CustomPaintingConfigItem> getAllMahGoodPaintings()
+    {
+        return mahPaintings;
+    }
+
+
+
+    /**
+     * <p>
+     * Returns a list with all files in the paintings config folder that are <b>not</b> already installed in the config file. (I bet you would never guess by the method name)
+     * </p>
+     * <p>
+     * The code only checks the file names, so it will return files that are not already in the config. If a file is configured with the wrong size or dimensions, it's still considered 'installed'.
+     * </p>
+     * <p>
+     * The list returned by this method is not persisted anywhere.
+     * </p>
+     * 
+     * @see sidben.ateliercanvas.client.gui.GuiScreenCustomPaintingsAddFileSelector
+     */
+    @SuppressWarnings("unchecked")
+    public static List<CustomPaintingConfigItem> getPaintingsInConfigFolderNotInstalled()
+    {
+        final List<CustomPaintingConfigItem> fakeConfigList = new ArrayList<CustomPaintingConfigItem>();
+
+
+        // Current installed paintings
+        final String[] currentFilesArray = new String[ConfigurationHandler.mahPaintings.size()];
+
+        for (int i = 0; i < ConfigurationHandler.mahPaintings.size(); i++) {
+            currentFilesArray[i] = ConfigurationHandler.mahPaintings.get(i).getPaintingFileName().toLowerCase();
+        }
+
+        final List<String> currentFiles = Arrays.asList(currentFilesArray);
+
+
+        // Sub-folder of the mod
+        final File folder = new File(Minecraft.getMinecraft().mcDataDir, ConfigurationHandler.IMAGES_BASE_PATH);
+
+        // Look all files in the folder, but filter by the valid extension types
+        for (final File fileEntry : folder.listFiles(new ImageFilenameFilter())) {
+            final String name = fileEntry.getName().toLowerCase();
+
+            // If the filename is not installed, adds to the return list
+            if (!currentFiles.contains(name)) {
+                final CustomPaintingConfigItem item = new CustomPaintingConfigItem(name, true, fileEntry.length(), name, "");
+                fakeConfigList.add(item);
+            }
+        }
+
+
+        return fakeConfigList;
+    }
+
+
+
+    public static void addNewItemAndSaveConfig(CustomPaintingConfigItem item)
+    {
+        if (item == null) {
+            return;
+        }
+        if (item.isValid()) {
+
+            // Adds the item
+            ConfigurationHandler.mahPaintings.add(item);
+            updateAndSaveConfig();
+
+        } else {
+            LogHelper.info("    Error importing a config entry: [" + item.getValiadtionErrors() + "]");
+        }
+    }
+
+
+
+    /**
+     * Clear current paintings config list and updates it with values from the current inner list.
+     */
+    public static void updateAndSaveConfig()
+    {
+
+        // Clear all content of the category
+        ConfigurationHandler.config.getCategory(CATEGORY_PAINTINGS).clear();
+
+        // Re-adds all the valid entries
+        Property configProp;
+        int c = 0;
+
+        for (final CustomPaintingConfigItem item : mahPaintings) {
+            final String configKey = String.format("%s_%03d", ConfigurationHandler.PAINTINGS_ARRAY_KEY, c);
+
+            configProp = ConfigurationHandler.config.get(CATEGORY_PAINTINGS, configKey, new String[] {});
+            configProp.set(item.ToStringArray());
+
+            c++;
+        }
+
+        // Saves the config file
+        ConfigurationHandler.config.save();
+    }
+
+
+
+    /**
+     * Looks on the current custom paintings installed for an element with the given UUID.
+     * 
+     * @param uuid
+     *            UUID
      * @return Config element of the given painting or NULL
      */
-    public static CustomPaintingConfigItem findPaintingByUUID(String uuid) {
-        if (!StringUtils.isNullOrEmpty(uuid) && mahPaintings.size() > 0)
-        {
-            for (CustomPaintingConfigItem item : mahPaintings) {
-                if (item.getUUID().equalsIgnoreCase(uuid)) {
+    public static CustomPaintingConfigItem findPaintingByUUID(UUID uuid)
+    {
+        if (uuid != null && mahPaintings.size() > 0) {
+            for (final CustomPaintingConfigItem item : mahPaintings) {
+                if (item.getUUID() == uuid) {
                     return item;
                 }
             }
         }
-        
+
         return null;
     }
 
 
     /**
-     * Looks on the current custom paintings installed for an element with the given UUID. 
+     * Looks on the current custom paintings installed for an element with the given UUID.
      * 
-     * @param uuid UUID
+     * @param uuid
+     *            UUID in string format
      * @return Config element of the given painting or NULL
      */
-    public static CustomPaintingConfigItem findPaintingByUUID(UUID uuid) {
-        return findPaintingByUUID (uuid.toString());
+    public static CustomPaintingConfigItem findPaintingByUUID(String uuidString)
+    {
+        UUID uuid = null;
+
+        try {
+            uuid = UUID.fromString(uuidString);
+        } catch (final IllegalArgumentException e) {
+            return null;
+        }
+
+        return findPaintingByUUID(uuid);
     }
 
-    
-    
+
+
     /**
-     * Returns a random item from the current installed custom paintings.
+     * Returns a random item from the current installed <b>and enabled</b> custom paintings.
      * 
      * @return Config element of a random painting or NULL
      */
-    public static CustomPaintingConfigItem getRandomPainting() {
-        if (mahPaintings.size() > 0)
-        {
-            // TODO: only search valid and enabled entries
+    public static CustomPaintingConfigItem getRandomPainting()
+    {
+        if (mahPaintings.size() > 0 && visiblePaintings.size() > 0) {
+            final Random rand = new Random();
             
-            Random rand = new Random();
-            int luckyDraw = rand.nextInt(mahPaintings.size());
-            return mahPaintings.get(luckyDraw);
+            // Draws a random entry from the array of enabled paintings
+            final int luckyDraw = rand.nextInt(visiblePaintings.size());
+            
+            // Returns the painting with the drawn UUID
+            return findPaintingByUUID(visiblePaintings.get(luckyDraw));
         }
-        
+
         return null;
     }
-    
+
 
 }

src/main/java/sidben/ateliercanvas/client/gui/GuiElementPaintingIconLoader.java
@@ -6,6 +6,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import javax.imageio.ImageIO;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
@@ -113,6 +115,9 @@ public abstract class GuiElementPaintingIconLoader extends GuiScreen
 
                     this._iconWidth = paintingIcon.getWidth();
                     this._iconHeight = paintingIcon.getHeight();
+                    
+                    // Updates the config entry with the dimensions 
+                    this._entryData.setSizePixels(_iconWidth, _iconHeight);
 
                     // Validate the max dimensions
                     if (this._iconWidth > ConfigurationHandler.maxPaintingSize || this._iconHeight > ConfigurationHandler.maxPaintingSize) {
@@ -131,7 +136,7 @@ public abstract class GuiElementPaintingIconLoader extends GuiScreen
                 e.printStackTrace();
 
             }
-
+            
         }
 
 
@@ -147,6 +152,8 @@ public abstract class GuiElementPaintingIconLoader extends GuiScreen
             this._validImage = true;
 
         }
+        
+        
     }
 
 
@@ -211,5 +218,24 @@ public abstract class GuiElementPaintingIconLoader extends GuiScreen
     {
         return "sidben.ateliercanvas.config.painting_info." + name;
     }
+    
+    
+    
+    
+    /**
+     * Extracts only the config elements of the given list of GuiElementPaintingIconLoader. 
+     */
+    public static List<CustomPaintingConfigItem> extractConfig(List<? extends GuiElementPaintingIconLoader> list) {
+        List<CustomPaintingConfigItem> configList = new ArrayList<CustomPaintingConfigItem>();
+        
+        for (final GuiElementPaintingIconLoader item : list) {
+            if (item != null && item._entryData != null) {
+                configList.add(item._entryData);
+            }
+        }
+        
+        return configList;
+    }
+    
 
 }

src/main/java/sidben/ateliercanvas/client/gui/GuiScreenCustomPaintingsAddFileSelector.java
@@ -12,9 +12,7 @@ import net.minecraft.client.gui.GuiOptionButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.util.StatCollector;
 import net.minecraft.util.Util;
-import net.minecraftforge.common.config.Property;
 import org.lwjgl.Sys;
-import scala.actors.threadpool.Arrays;
 import sidben.ateliercanvas.handler.ConfigurationHandler;
 import sidben.ateliercanvas.handler.CustomPaintingConfigItem;
 import sidben.ateliercanvas.helper.ImageFilenameFilter;
@@ -83,27 +81,13 @@ public class GuiScreenCustomPaintingsAddFileSelector extends GuiScreen implement
         this.displayDetailsButtons(false);
 
 
-        // Current installed paintings
-        final String[] currentFilesArray = new String[ConfigurationHandler.mahPaintings.size()];
-
-        for (int i = 0; i < ConfigurationHandler.mahPaintings.size(); i++) {
-            currentFilesArray[i] = ConfigurationHandler.mahPaintings.get(i).getPaintingFileName().toLowerCase();
-        }
-
-        final List<String> currentFiles = Arrays.asList(currentFilesArray);
-
 
         // Listbox data (loads the image files that are not already in the mod config)
+        final List<CustomPaintingConfigItem> filesNotInstalled = ConfigurationHandler.getPaintingsInConfigFolderNotInstalled();
         this.paintingList = new ArrayList();
 
-        final File folder = new File(this.mc.mcDataDir, ConfigurationHandler.IMAGES_BASE_PATH);
-        for (final File fileEntry : folder.listFiles(new ImageFilenameFilter())) {
-            final String name = fileEntry.getName().toLowerCase();
-            if (!currentFiles.contains(name)) {
-                // Creates a fake CustomPaintingConfigItem item to display in the listbox
-                final CustomPaintingConfigItem item = new CustomPaintingConfigItem(name, true, fileEntry.length(), name, "");
-                this.paintingList.add(new GuiElementPaintingListEntry(this, item));
-            }
+        for (final CustomPaintingConfigItem configItem : filesNotInstalled) {
+            this.paintingList.add(new GuiElementPaintingListEntry(this, configItem));
         }
 
 
@@ -297,42 +281,9 @@ public class GuiScreenCustomPaintingsAddFileSelector extends GuiScreen implement
 
         LogHelper.info("Importing painting #" + index + " from the list");
         if (index >= 0 && index < this.paintingList.size()) {
-
-            // TODO: encapsulate a method to add paintings via CustomPaintingConfigItem
             final CustomPaintingConfigItem selectedEntry = this.paintingList.get(index)._entryData;
-
-            if (selectedEntry.isValid()) {
-                ConfigurationHandler.mahPaintings.add(selectedEntry);
-
-                // TODO: Really, really encapsulate this mess (the code should only interact with mahPaintings, config handler should... handle the config)
-                final String configID = ConfigurationHandler.CATEGORY_PAINTINGS;
-
-                // Clear all content of the category
-                ConfigurationHandler.config.getCategory(configID).clear();
-
-                // Re-adds all the valid entries
-                Property configProp;
-                int c = 0;
-
-                for (final CustomPaintingConfigItem item : ConfigurationHandler.mahPaintings) {
-                    final String configKey = String.format("%s_%03d", ConfigurationHandler.PAINTINGS_ARRAY_KEY, c);
-
-                    configProp = ConfigurationHandler.config.get(configID, configKey, new String[] {});
-                    configProp.set(item.ToStringArray());
-
-                    c++;
-                }
-
-                // Saves the config file
-                ConfigurationHandler.config.save();
-
-
-                LogHelper.info("    Successfuly imported a config entry: [" + selectedEntry.toString() + "]");
-                return true;
-            } else {
-                LogHelper.info("    Error importing a config entry: [" + selectedEntry.getValiadtionErrors() + "]");
-            }
-
+            ConfigurationHandler.addNewItemAndSaveConfig(selectedEntry);
+            return true;
 
         } else {
             LogHelper.error("    Invalid painting index");
@@ -383,13 +334,15 @@ public class GuiScreenCustomPaintingsAddFileSelector extends GuiScreen implement
         this.selectedIndex = index;
     }
 
-    
+
     /**
      * Displays the GUI element with the image details and thumbnail.
      * 
-     * @param index Index of the element on the listbox.
+     * @param index
+     *            Index of the element on the listbox.
      */
-    public void displayDetails(int index) {
+    public void displayDetails(int index)
+    {
         if (index >= 0 && index < this.paintingList.size()) {
             final GuiElementPaintingListEntry entry = this.paintingList.get(index);
             this.guiElementPaintingDetails.updateConfigItem(entry._entryData);

src/main/java/sidben/ateliercanvas/client/gui/GuiScreenCustomPaintingsManage.java
@@ -8,7 +8,6 @@ import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiOptionButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.util.StatCollector;
-import net.minecraftforge.common.config.Property;
 import sidben.ateliercanvas.handler.ConfigurationHandler;
 import sidben.ateliercanvas.handler.CustomPaintingConfigItem;
 import sidben.ateliercanvas.reference.ColorTable;
@@ -106,7 +105,7 @@ public class GuiScreenCustomPaintingsManage extends GuiScreen implements IListCo
 
         // Listbox data (loads from config)
         this.paintingList = new ArrayList();
-        for (final CustomPaintingConfigItem item : ConfigurationHandler.mahPaintings) {
+        for (final CustomPaintingConfigItem item : ConfigurationHandler.getAllMahGoodPaintings()) {
             this.paintingList.add(new GuiElementPaintingListEntry(this, item));
         }
 
@@ -183,28 +182,10 @@ public class GuiScreenCustomPaintingsManage extends GuiScreen implements IListCo
                 }
 
 
-                // TODO: For realz, clean this mess
                 // TODO: Only save if there were changes
-
-                // Clear all content of the category
-                ConfigurationHandler.config.getCategory(configID).clear();
-
-                // Re-adds all the valid entries
-                Property configProp;
-                int c = 0;
-
-                for (final GuiElementPaintingListEntry item : this.paintingList) {
-                    final String configKey = String.format("%s_%03d", ConfigurationHandler.PAINTINGS_ARRAY_KEY, c);
-
-                    configProp = ConfigurationHandler.config.get(configID, configKey, new String[] {});
-                    configProp.set(item._entryData.ToStringArray());
-
-                    c++;
-                }
-
-                // Saves the config file
-                ConfigurationHandler.config.save();
-
+                // Save the changes to the config file
+                // List<CustomPaintingConfigItem> configEntries = GuiElementPaintingIconLoader.extractConfig(this.paintingList);
+                ConfigurationHandler.updateAndSaveConfig();
 
 
                 // Returns to the parent screen
@@ -213,10 +194,10 @@ public class GuiScreenCustomPaintingsManage extends GuiScreen implements IListCo
 
             } else if (button.id == BT_ID_ADDNEW) {
                 this.mc.displayGuiScreen(new GuiScreenCustomPaintingsAdd(this));
-            
-            } 
-            
-            
+
+            }
+
+
             else if (button.id == BT_ID_CHANGE) {
                 if (button.enabled && this.selectedIndex >= 0 && this.selectedIndex < this.paintingList.size()) {
                     final GuiElementPaintingListEntry entry = this.paintingList.get(this.selectedIndex);
@@ -225,11 +206,10 @@ public class GuiScreenCustomPaintingsManage extends GuiScreen implements IListCo
                     }
                 }
                 // TODO: Add the editor callback logic (action confirmed)
-    
+
             }
 
-        
-            
+
 
         }
     }
@@ -294,14 +274,16 @@ public class GuiScreenCustomPaintingsManage extends GuiScreen implements IListCo
         this.displayDetails(index);
         this.selectedIndex = index;
     }
-    
-    
+
+
     /**
      * Displays the GUI element with the image details and thumbnail.
      * 
-     * @param index Index of the element on the listbox.
+     * @param index
+     *            Index of the element on the listbox.
      */
-    public void displayDetails(int index) {
+    public void displayDetails(int index)
+    {
         if (index >= 0 && index < this.paintingList.size()) {
             final GuiElementPaintingListEntry entry = this.paintingList.get(index);
             this.guiElementPaintingDetails.updateConfigItem(entry._entryData);
@@ -310,8 +292,8 @@ public class GuiScreenCustomPaintingsManage extends GuiScreen implements IListCo
                     .translateToLocal(getLanguageKey("disabled"));
 
             this.displayDetailsButtons(true);
-        }        
+        }
     }
-    
+
 
 }

commit c7bf18bad64a001c5a1b8afff73373d041704458

Fix for the rendering issue introduced in 183dec5e13570bbe9adc4ea03cf975bc4700273b
Also removed duplicate UUID entries when updating the config list.

This removal, together with changes made on
9931f40f01c7625ea8e25fb4abd48417ae34d9c0 managed to fix #8, that was
caused by the client loading the wrong image size.

src/main/java/sidben/ateliercanvas/handler/ConfigurationHandler.java
@@ -314,20 +314,37 @@ public class ConfigurationHandler
      */
     public static void updateAndSaveConfig()
     {
+        List<UUID> uniqueEntries = new ArrayList<UUID>();
+        
         // Clear all content of the category
         ConfigurationHandler.config.getCategory(CATEGORY_PAINTINGS).clear();
+        visiblePaintings.clear();
 
         // Re-adds all the valid entries
         Property configProp;
         int c = 0;
 
         for (final CustomPaintingConfigItem item : mahPaintings) {
-            final String configKey = String.format("%s_%03d", ConfigurationHandler.PAINTINGS_ARRAY_KEY, c);
+            // Check for duplicate UUIDs
+            if (!uniqueEntries.contains(item.getUUID())) {
 
-            configProp = ConfigurationHandler.config.get(CATEGORY_PAINTINGS, configKey, new String[] {});
-            configProp.set(item.ToStringArray());
+                // Updates the config
+                final String configKey = String.format("%s_%03d", ConfigurationHandler.PAINTINGS_ARRAY_KEY, c);
+                
+                configProp = ConfigurationHandler.config.get(CATEGORY_PAINTINGS, configKey, new String[] {});
+                configProp.set(item.ToStringArray());
+                
+                // Updates the visible paintings array
+                if (item.getIsEnabled()) {
+                    visiblePaintings.add(item.getUUID());
+                }
 
-            c++;
+                uniqueEntries.add(item.getUUID());
+                c++;
+                
+            }
+            
+            // TODO: remove the duplicate entries from mahPaintings
         }
 
         // Saves the config file
@@ -347,7 +364,7 @@ public class ConfigurationHandler
     {
         if (uuid != null && mahPaintings.size() > 0) {
             for (final CustomPaintingConfigItem item : mahPaintings) {
-                if (item.getUUID() == uuid) {
+                if (item.getUUID().equals(uuid)) {
                     return item;
                 }
             }

src/main/java/sidben/ateliercanvas/item/ItemRandomPainting.java
@@ -2,7 +2,6 @@ package sidben.ateliercanvas.item;
 
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;